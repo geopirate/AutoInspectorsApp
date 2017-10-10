edbd2e65e2b6680d6cafe98bbb6c9e1e8435cfd8
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-10 11:49:24 -0700

    All 5 tests pass

--- Tests/UnitTests.Tests/UnitTest1.cs
+++ Tests/UnitTests.Tests/UnitTest1.cs
@@ -22,33 +22,6 @@ namespace UnitTests.Tests
             Assert.Equal("Audi", m.Make);
         }
 
-
-        [Fact]
-        public void YearIsInRange()
-        {
-            // Arrange
-            var y = new Vehicle { Year = 2017 };
-
-            //Act
-            y.Year = 2200;
-
-            //Assert
-            Assert.NotInRange(2200, 1900, 2100);
-        }
-
-        [Fact]
-        public void LicensePlateIsString()
-        {
-            // Arrange
-            var p = new Vehicle { LicensePlate = "UWATM8" };
-
-            //Act
-            p.LicensePlate = "AYYLMAO";
-
-            //Assert
-            Assert.IsType<string>(p.LicensePlate);
-        }
- 
         [Fact]
         public void HomeIndexResultIsView()
         {
@@ -64,7 +37,44 @@ namespace UnitTests.Tests
             Assert.IsType<ViewResult>(result);
         }
 
+        [Fact]
+        public void LicensePlateIsString()
+        {
+            // Arrange
+            var p = new Vehicle { LicensePlate = "UWATM8" };
 
+            //Act
+            p.LicensePlate = "AYYLMAO";
+
+            //Assert
+            Assert.IsType<string>(p.LicensePlate);
+        }
+
+        [Fact]
+        public void YearNotInRange()
+        {
+            // Arrange
+            var y = new Vehicle { Year = 2017 };
+
+            //Act
+            y.Year = 2200;
+
+            //Assert
+            Assert.NotInRange(2200, 1900, 2100);
+        }
+
+        [Fact]
+        public void YearIsInRange()
+        {
+            // Arrange
+            var y = new Vehicle { Year = 2017 };
+
+            //Act
+            y.Year = 2000;
+
+            //Assert
+            Assert.InRange(2000, 1900, 2100);
+        }
 
     }
 }
